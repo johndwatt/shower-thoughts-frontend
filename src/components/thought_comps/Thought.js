70f447d2e911df927dc90227054f0780405b7191
johndwatt <jdw23@protonmail.com>
2022-03-23 16:51:54 -0700

    properly displays thought data and adds basic temp styling

--- src/components/thought_comps/Thought.js
+++ src/components/thought_comps/Thought.js
@@ -5,9 +5,9 @@ import "../../styles/Thought.css"
 function Thought(props) {
     return (
         <div className="thought">
-            <p className="thought-content">test thought</p>
+            <p className="thought-content">{props.thought.content}</p>
             <div className='thought-flex'>
-                <p className="thought-thinker">test thinker</p>
+                <p className="thought-thinker">{props.thought.thinker}</p>
             </div>
         </div>
     );

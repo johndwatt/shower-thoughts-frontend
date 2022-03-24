68ba79e47fa5517e0075b81dd6da4d353b2be475
johndwatt <jdw23@protonmail.com>
2022-03-24 14:40:58 -0700

    adds working random thought to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { API_URL } from "../api/api_connection"
-// import axios from 'axios';
+import axios from 'axios';
 
-import "../styles/ThoughtContainer.css"
+import Thought from '../components/thought_comps/Thought';
 
-// const url = API_URL + '/home'
+const url = API_URL + '/home'
 
 function Home(props) {
+    const [randThought, setRandThought] = useState({});
 
-    // need to add queries for random and latest thought and have those display below
+    const randUrl = url + "/random"
+
+    useEffect(() => {
+        axios.get(randUrl)
+            .then(response => {
+                setRandThought(response.data.randomThought[0]);
+            }).catch(error => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div className='home-container'>
@@ -16,10 +26,11 @@ function Home(props) {
             <h3>A place to share your shower thoughts.</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt perferendis nobis iste, provident molestiae eius aut rerum iusto iure excepturi repellendus voluptates ut minus quis architecto ratione consequuntur vel eligendi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio debitis fuga esse iure consequatur cum quis, rem saepe corporis ipsa expedita maiores sint voluptatum tenetur officia molestias dignissimos nostrum animi.</p>
             <div className='random-thought'>
-                <p>Random thought stuff goes here</p>
+                <h3>Random Shower Thought</h3>
+                <Thought key={randThought._id} thought={randThought} />
             </div>
             <div className='latest-thought'>
-                <p>Latest thought stuff goes here</p>
+                <p>Latest thought goes here</p>
             </div>
         </div>
 

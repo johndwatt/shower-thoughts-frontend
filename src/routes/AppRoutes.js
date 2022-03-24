4efd18dba92aede30c521abbce6ab627c45585b1
johndwatt <jdw23@protonmail.com>
2022-03-23 18:01:00 -0700

    adds create page and route with content

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import Dashboard from '../pages/Dashboard';
 import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
+import Create from '../pages/Create';
 
 function AppRoutes(props) {
     return (
         <Routes>            
-            <Route exact path='/'  element={<Home />} /> 
-            <Route exact path='/dashboard'  element={<Dashboard />} />
+            <Route exact path='/' element={<Home />} /> 
+            <Route exact path='/dashboard' element={<Dashboard />} />
+            <Route exact path='/dashboard/add' element={<Create />} />
         </Routes>            
     );
 }

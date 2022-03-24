220ea504cd3f6e5e7e81eac908baa53e635161fa
johndwatt <jdw23@protonmail.com>
2022-03-23 17:12:40 -0700

    adds content and styling to navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AppRoutes from './routes/AppRoutes'
+import Navbar from "./components/nav_comps/Navbar";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AppRoutes />
     </div>
   );

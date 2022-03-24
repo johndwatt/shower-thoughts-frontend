f3d160d69067c5e7ef7a335542f849a5952c5c20
johndwatt <jdw23@protonmail.com>
2022-03-24 15:01:11 -0700

    adds refresh button with loading animation for randThought

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,6 +29,19 @@ function Home(props) {
             });
     }, []);
 
+    const handleRandRefresh = async (e) => {
+        e.preventDefault();
+
+        setRandThought(null);
+
+        await axios.get(randUrl)
+            .then(response => {
+                setRandThought(response.data.randomThought[0]);
+            }).catch(error => {
+                console.log(error);
+            });
+    };
+
     return (
         <div className='home-container'>
             <h1>Welcome to Shower Thought Central</h1>
@@ -36,7 +49,12 @@ function Home(props) {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt perferendis nobis iste, provident molestiae eius aut rerum iusto iure excepturi repellendus voluptates ut minus quis architecto ratione consequuntur vel eligendi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio debitis fuga esse iure consequatur cum quis, rem saepe corporis ipsa expedita maiores sint voluptatum tenetur officia molestias dignissimos nostrum animi.</p>
             <div className='random-thought'>
                 <h3>Random Shower Thought</h3>
-                <Thought key={randThought._id} thought={randThought} />
+                {randThought !== null ? (
+                    <Thought key={randThought._id} thought={randThought} />
+                ) : (
+                    <h3 className='loading'>Loading...</h3>
+                )}
+                <button onClick={handleRandRefresh}>Randomize!</button>
             </div>
             <div className='latest-thought'>
                 <h3>Latest Shower Thought</h3>

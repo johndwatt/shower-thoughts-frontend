1f1ae0890456a678af3aab453485fa98bb89fe96
johndwatt <jdw23@protonmail.com>
2022-03-24 17:57:13 -0700

    adds front end search functionality to thought container

--- src/components/thought_comps/ThoughtContainer.js
+++ src/components/thought_comps/ThoughtContainer.js
@@ -9,6 +9,7 @@ const url = API_URL + '/thoughts'
 
 function ThoughtContainer(props) {
     const [thoughts, setThoughts] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         axios.get(url)
@@ -19,15 +20,66 @@ function ThoughtContainer(props) {
             });
     }, []);
 
+    const handleSearchInput = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleClearSearch = (e) => {
+        e.preventDefault();
+
+        setSearch("")
+
+        axios.get(url)
+            .then(response => {
+                setThoughts(response.data.thoughts);
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+        
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        axios.get(url + "?search=" + search)
+        .then(response => {
+            setThoughts(response.data.thoughts);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+
     return (
         <div className='thought-container'>
-            {thoughts.length > 1 ? (
-                thoughts.map(thought => ( 
-                    <Thought key={thought._id} thought={thought} />
-                ))
-            ) : (
-                <h3 className='loading'>Loading...</h3>
-            )}
+            <div className='thought-container-search'>
+                <form>
+                    <input 
+                        name="search" 
+                        type="search"
+                        value={search}
+                        onChange={handleSearchInput}
+                        placeholder="Search" />
+                    <button 
+                        type="submit" 
+                        onClick={handleSearch}>
+                        Search
+                    </button>
+                    <button 
+                        onClick={handleClearSearch}>
+                        Clear
+                    </button>
+                </form>
+            </div>
+            <div className='thought-container-display'>
+                {thoughts.length > 0 ? (
+                    thoughts.map(thought => ( 
+                        <Thought key={thought._id} thought={thought} />
+                    ))
+                ) : (
+                    <h3>No thoughts found. Click "Add Thought" to create a shower thought!</h3>
+                    // ISSUE: What to do about loading?
+                )}
+            </div>
         </div>
 
     );
